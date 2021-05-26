6737f6600bf01bd59aa55820f799819730a39a44
RenanKabbach <renanbotarelli@gmail.com>
2021-05-26 11:45:11 -0300

    Ajuste no fromato da data

--- scripts.js
+++ scripts.js
@@ -116,7 +116,7 @@ const Utils = {
 
   formatDate(date) {
     const splittedDate = date.split("-");
-    return `${splittedDate[2]}/${splittedDate[1]}${splittedDate[0]}`;
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
   },
 
   formatCurrency(value) {
